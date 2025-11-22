27d99b5bd46faf90d94b0c0e48bbb8ae6b1b78f7
6zeropk <nemm3623@naver.com>
2025-11-23 06:06:17 +0900

    [refactor] Invalid Category 예외 수정 및 Category.contain 메소드 추가

--- src/main/java/com/example/Server/global/type/Category.java
+++ src/main/java/com/example/Server/global/type/Category.java
@@ -1,5 +1,7 @@
 package com.example.Server.global.type;
 
+import java.util.Arrays;
+
 public enum Category {
     STUDY,      // 학업
     JOB,        // 직장
@@ -8,5 +10,9 @@ public enum Category {
     HEALTH,     // 건강
     RELATION,   // 관계
     FINANCE,    // 경제
-    ETC         //  기타
+    ETC;         //  기타
+
+    public static boolean contains(Category category) {
+        return Arrays.stream(Category.values()).anyMatch(v -> v.equals(category));
+    }
 }
